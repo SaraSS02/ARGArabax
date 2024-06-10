82e0214583bd05eeeb4f9f9adadbc2f632a08c29
SaraSS02 <s285050@studenti.polito.it>
2024-06-10 17:23:38 +0200

    klynt

---
+++ publish-to-webCEO/miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "launchAppWording": "Then Launch Project",
+    "title": "ARG_CEO",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "description": "",
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "always",
+    "yesWording": "Yes",
+    "url": "",
+    "noWording": "No",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "analyticsKey": "",
+    "downloadAppWording": "Download App"
+}
